d82f677db0c243ed41587edb120447962388a0da
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2024-09-01 21:07:15 +0300

    change themew

--- init.lua
+++ init.lua
@@ -888,7 +888,7 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --
 
-    'sainnhe/gruvbox-material',
+    'tokyonight.nvim',
     -- 'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
@@ -896,7 +896,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.o.background = 'dark'
-      vim.cmd.colorscheme 'gruvbox-material'
+      vim.cmd.colorscheme 'tokyonight-storm'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
